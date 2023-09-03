780b0de4c807f98069443cb4c5bcc8bbd4eae010
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2023-09-03 12:26:23 -0300

    list and show customers

---
+++ app/app.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+  "fmt"
+  "github.com/eduardoyutaka/banking/domain"
+  "github.com/eduardoyutaka/banking/service"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+  "os"
+)
+
+func sanityCheck() {
+  if os.Getenv("SERVER_ADDRESS") == "" ||
+     os.Getenv("SERVER_PORT") == "" {
+    log.Fatal("Environment variable not defined...")
+  }
+}
+
+func Start() {
+  sanityCheck()
+
+	router := mux.NewRouter()
+  ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryDb())}
+	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
+  router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
+
+  address := os.Getenv("SERVER_ADDRESS")
+  port := os.Getenv("SERVER_PORT")
+
+  log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "github.com/eduardoyutaka/banking/app"
+  "github.com/eduardoyutaka/banking/logger"
+)
+
+func main() {
+  logger.Info("Starting the application...")
+	app.Start()
+}
